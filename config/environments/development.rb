de8179e177e64653802028740e2e65a276dc9f61
Mehul Kar <mehul.kar@gmail.com>
2012-03-07 16:25:09 -0800

    Devise integrated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Todone::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # Added because devise told me so
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
 end
