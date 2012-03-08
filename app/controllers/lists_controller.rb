d1a6b6eaeb011a2e3c44025bcbff544ee19a47aa
jason1098 <jasonbushis@gmail.com>
2012-03-07 19:00:56 -0800

    added ability to add lists to user

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,23 @@
 class ListsController < ApplicationController
   before_filter :authenticate_user!
   
-  def index
-    
+  def new
   end
   
+  def index
+    @list = List.new
+  end
+
+  
+  def create
+    @list = current_user.lists.build(params[:list])
+    if @list.save
+      flash[:success] = "New To Do List created!"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Woops! Something went wrong.. :("
+      render :index
+    end
+  end
+
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ActiveRecord::Base
   has_many :tasks
   belongs_to :user
   validates :user_id, presence: true
+  validates :name, presence: true
 end
