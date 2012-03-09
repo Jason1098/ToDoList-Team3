cce0b3371cfc17d15363c12715c36a09a4205368
Jason Bush <jasonbushis@gmail.com>
2012-03-09 07:47:00 -0800

    updated initializer omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-  provider :facebook, 'APP_ID', 'APP_SECRET'
+  provider :facebook, '350915281619900', '9d712da80a7b993551bbb690698bd2f1'
 end
