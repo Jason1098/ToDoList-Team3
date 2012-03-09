b08146a32c5894520d3fd217f3f4e94d5b74bbd1
Lachy Groom <lachygroom+collectivejam@gmail.com>
2012-03-08 17:47:11 -0800

    Added task completed/not completed toggle buttons

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,10 @@
 module ListsHelper
+  def task_completed_button_text state
+    state ? "Not done" : "Done"
+  end
+  
+  def task_completed_button_style state
+    state ? "btn btn-warning" : "btn btn-success"
+  end
+  
 end
