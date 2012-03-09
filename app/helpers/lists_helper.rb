b66d13ecf4d4af3f17b628c04aa749b0ad45cb4b
Lachy Groom <lachygroom+collectivejam@gmail.com>
2012-03-08 17:53:01 -0800

    Fixed button toggles by reversing

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,10 +1,10 @@
 module ListsHelper
   def task_completed_button_text state
-    state ? "Not done" : "Done"
+    state ? "Done" : "Not done"
   end
   
   def task_completed_button_style state
-    state ? "btn btn-warning" : "btn btn-success"
+    state ? "btn btn-success" : "btn btn-warning"
   end
   
 end
